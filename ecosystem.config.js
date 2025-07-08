be644907552899eb38f49386f7120994b727f902
Josue Henrique <josue21henrique@gmail.com>
2025-07-08 11:02:47 -0300

    fix: corrigindo erro de servidor reiniciando apos cadastrar imagem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 NODE_ENV: "development"
             },
             watch: true,
-            ignore_watch: ["node_modules", "logs", "uploads"],
+            ignore_watch: ["node_modules", "logs", "dist/uploads"],
             error_file: "logs/error.log",
             out_file: "logs/out.log",
             log_date_format: "YYYY-MM-DD HH:mm:ss"
